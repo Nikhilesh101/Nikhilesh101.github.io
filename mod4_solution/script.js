da79a705ef9153433f2d9df944dca33f7f1a61e9
Nikhilesh101 <nikhilesh.gattu8@gmail.com>
2023-06-26 20:45:49 +0530

    updated

--- mod4_solution/script.js
+++ mod4_solution/script.js
@@ -1,13 +1,21 @@
-(function (window) {
-  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names=new Array();
+names[0]="Nikhilesh";
+names[1]="Sri";
+names[2]="Satish";
+names[3]="Vardhan";
+names[4]="Lasya";
+names[5]="Sid";
+names[6]="Sam";
+names[7]="Sus";
+names[8]="Suj";
+names[9]="tan";
 
-  for (var i in names) {
-    var firstLetter = names[i].charAt(0).toLowerCase();
-    if (firstLetter === 'j') {
-      byeSpeaker.speak(names[i]);
-    } else {
-      helloSpeaker.speak(names[i]);
-    }
-  }
-})(window);
 
+for (var i = 0; i < names.length; i++) {
+	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
+        console.log("Goodbye "+ names[i])
+	}
+	else{
+		console.log("Hello "+ names[i])
+	}
+}
